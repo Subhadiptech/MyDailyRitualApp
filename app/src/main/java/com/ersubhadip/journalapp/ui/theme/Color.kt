3cac05f5a27a143de559fcbc4e9cd5cfaedf06f5
Subhadiptech <subhadipdhn@gmail.com>
2023-12-10 17:09:38 +0530

    theming done

--- app/src/main/java/com/ersubhadip/journalapp/ui/theme/Color.kt
+++ app/src/main/java/com/ersubhadip/journalapp/ui/theme/Color.kt
@@ -2,10 +2,9 @@ package com.ersubhadip.journalapp.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
-
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Black = Color(0xFF000000)
+val White = Color(0xFFFFFFFF)
+val PrimaryPink = Color(0xFFfccad0)
+val PrimaryBlue = Color(0xFFcde6f9)
+val PrimaryPurple = Color(0xFFedd0ff)
+val PrimaryYellow = Color(0xFFffe1b3)
